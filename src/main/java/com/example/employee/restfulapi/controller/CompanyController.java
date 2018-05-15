716ce4acb0fee2867b6656dccd835fe2c52876f5
summer2023 <happyxia1023@163.com>
2018-05-15 14:38:49 +0800

    write companyrepository and companyController

--- src/main/java/com/example/employee/restfulapi/controller/CompanyController.java
+++ src/main/java/com/example/employee/restfulapi/controller/CompanyController.java
@@ -1,7 +1,8 @@
 package com.example.employee.restfulapi.controller;
 
+import com.example.employee.restfulapi.entity.Company;
 import com.example.employee.restfulapi.entity.Employee;
-import com.example.employee.restfulapi.repository.EmployeeRepository;
+import com.example.employee.restfulapi.repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -9,41 +10,39 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/employee")
-public class EmployeeController {
+public class CompanyController {
     @Autowired
-    private EmployeeRepository employeeRepository;
+    private CompanyRepository companyRepository;
 
     @GetMapping
-    public @ResponseBody Iterable<Employee> findAllEmployees(){return employeeRepository.findAll();}
+    public @ResponseBody Iterable<Company> findAllCompanys(){return companyRepository.findAll();}
 
     @GetMapping(value = "/{id}")
-    public Employee getEmployee(@PathVariable Long id) {
-        return employeeRepository.findById(id);
+    public Company getCompany(@PathVariable Long id) {
+        return companyRepository.findById(id);
     }
+
+    @GetMapping(value = "/{id}")
+    public List<Employee> getEmployee(@PathVariable Long id){return companyRepository.findEmployeeByCompanyId(id);}
 
     @GetMapping(value = "/page/{page}/pageSize/{pageSize}")
-    public Page<Employee> getEmployees(@PathVariable int page, @PathVariable int pageSize) {
-        return employeeRepository.findAll(new PageRequest(page, pageSize));
+    public Page<Company> getCompanys(@PathVariable int page, @PathVariable int pageSize) {
+        return companyRepository.findAll(new PageRequest(page, pageSize));
     }
 
-    @GetMapping(value = "/male")
-    public List<Employee> getMaleEmployee(){return employeeRepository.findByGender("male");}
-
     @PostMapping
-    public Employee saveEmployee(Employee employee) {
-        return employeeRepository.save(employee);
+    public Company saveCompany(Company company) {
+        return companyRepository.save(company);
     }
 
     @PutMapping(value = "/{id}")
-    public Employee updateEmployee(@PathVariable Long id, @ModelAttribute Employee employee) {
-        employeeRepository.updateById(id, employee.getName(), employee.getAge(), employee.getGender(), employee.getSalary(), employee.getCompanyId());
-        return employeeRepository.findById(id);
+    public Company updateCompany(@PathVariable Long id, @ModelAttribute Company company) {
+        companyRepository.updateById(id,company.getCompanyName(),company.getEmployeesNumber());
+        return companyRepository.findById(id);
     }
 
     @DeleteMapping(value = "/{id}")
     public void deleteEmployee(@PathVariable Long id) {
-        employeeRepository.deleteById(id);
+        companyRepository.deleteById(id);
     }
 }
--- src/main/java/com/example/employee/restfulapi/repository/CompanyRepository.java
+++ src/main/java/com/example/employee/restfulapi/repository/CompanyRepository.java
@@ -1,9 +1,36 @@
 package com.example.employee.restfulapi.repository;
 
 import com.example.employee.restfulapi.entity.Company;
+import com.example.employee.restfulapi.entity.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
+    List<Company> findAll();
+
+    Company findById(Long id);
+
+    @Query("select e from Employee e where e.companyId=?1")
+    List<Employee> findEmployeeByCompanyId(Long id);
+
+    Page<Company> findAll(Pageable pageable);
+
+    Company save(Company company);
+
+    @Modifying
+    @Transactional
+    @Query("update Company c set c.companyName = ?2, c.employeesNumber = ?3 where c.id = ?1")
+    int updateById(Long id, String companyName, String employeesNumber);
+
+    @Transactional
+    void deleteById(Long id);
 }
